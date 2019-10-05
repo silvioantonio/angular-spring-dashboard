d36b37dfa201482a3aa9f9863c7ab86919841e35
Silvio Antonio <oliveira_0000@hotmail.com>
2019-10-05 17:51:55 -0300

    Criados PUT e GET do usuario, GET por ID e PUT por ID, Alterado retorno para devidas requisições HTTP

--- dashboard/src/main/java/com/silvio/controller/UsuarioController.java
+++ dashboard/src/main/java/com/silvio/controller/UsuarioController.java
@@ -7,10 +7,13 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,19 +32,46 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
 	public List<Usuario> listarUsuarios(){
 		return usuarioRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Usuario getUsuario(@Valid @PathVariable Long id) {
+		Optional<Usuario> usuario = usuarioRepository.findById( id);
+		
+		if(usuario.get() == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID do Usuario não encontrado");
+		}
+		return usuario.get();
+	}
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	private Usuario adicionarUsuario(@Valid @RequestBody Usuario usuario) {
+	public Usuario adicionarUsuario(@Valid @RequestBody Usuario usuario) {
 		Optional<Usuario> usuarioExistente = usuarioRepository.findByNome(usuario.getNome());
 		if(usuarioExistente.isPresent()) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
 					"Já existe um usuario com esse nome");
 		}
 		return usuarioRepository.save(usuario);
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario alterarUsuario(@RequestBody Usuario novoUsuario, @Valid @PathVariable Long id) {
+		try {
+				return usuarioRepository.findById(id)
+						.map(usuario -> {
+					    	usuario.setNome(novoUsuario.getNome());
+					        return usuarioRepository.save(usuario);
+					      }).get();
+		}catch(java.util.NoSuchElementException nsee) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe usuario com esse ID");
+		}
+		
 	}
 	
 }
